86e281171a1950dbe186c3722610b612185b9ab0
zhaozelong <zhaozelong@leagsoft.com>
2024-06-06 20:33:10 +0800

    add:first commit

---
+++ ex1.c
@@ -0,0 +1,14 @@
+//
+// @Author ZhaoZelong
+// @Date 2024/06/06
+// @Email zhaozelong@leagsoft.com
+// @Copyright (c) 2024 LeagSoft Inc.
+// @Description
+//
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    puts("Hello world.");
+    puts("\033[31mThis text will be red.\033[0m");
+    return 0;
+}
